aee27375e525f6db04136bc3b14f4d8d589b8b14
ClaireGuerin <guerin.claire01@gmail.com>
2021-06-28 18:56:01 +0200

    data types and conditions

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+// Include the string library
+#include <string>
+// Include the cmath library to use math functions such as sqrt, log or round
+#include <cmath>
 using namespace std;
 
 /* The code below will print the words Hello World!
@@ -8,8 +12,16 @@ to the screen, and it is amazing */
 int main() {
   const int myNum = 15; // define variable with type int. Make it constant: it can never change
   double myBool; // declare variable without assigned value
+  float f1 = 35e3;
+  double d1 = 12E4;
+  /* Note on double VS float: 
+  The precision of a floating point value indicates how many digits the value can have after the decimal point. 
+  The precision of float is only six or seven decimal digits, while double variables have a precision of about 15 digits. 
+  Therefore it is safer to use double for most calculations. */
+  char a = 65, b = 66, c = 67; // will return 'A', 'B', 'C'
   myBool = true;
   int x = 5, y = 6, z = 50; // declare multiple vars of same type at once
+  string myString = "hola";
   
 
   // static_cast<double>(myNum) // this forces a type change from a int variable to double. It is usually considered bad practice.
@@ -27,6 +39,34 @@ int main() {
   cin >> inputNum2;
   const int sum = inputNum1 + inputNum2;
   cout << "Sum is: " << sum;
+
+  string firstName = "John ";
+  string lastName = "Doe";
+  string fullName = firstName.append(lastName); // or string fullName = firstName + lastName;
+  cout << fullName + "\n"; 
+  cout << "The name " + fullName + "contains " << fullName.length() << " letters\n"; // or fullName.size()
+  cout << fullName + "has initials " + firstName[0] + lastName[0];
+
+  string userName;
+  cout << "Type your full name: ";
+  getline (cin, userName); // a regular cin >> userName would only return the first string before space, e.g. Claire instead of Claire Guerin
+  cout << "Your name is: " << userName;
+
+  cout << sqrt(64);
+  cout << round(2.6);
+  cout << log(2);
+
+  if (x < y) {
+    cout << "x is greater than y";
+  } else if (x == y) {
+    cout << "x is y";
+  } else {
+    cout << "x is smaller than y";
+  }
+
+  int time = 20;
+  string result = (time < 18) ? "Good day." : "Good evening."; // ternary operator = short hand for if... else statement
+  cout << result; 
 
   return 0; 
 }
