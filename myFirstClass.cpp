0b66c5aff12cf508f2a4507b882f13860adda226
ClaireGuerin <guerin.claire01@gmail.com>
2021-06-29 14:33:33 +0200

    created new file for classes

---
+++ myFirstClass.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// Include the string library
+#include <string>
+// Include the cmath library to use math functions such as sqrt, log or round
+#include <cmath>
+using namespace std;
+
+// C++ is Object Oriented Programming
+
+/* Object-oriented programming has several advantages over procedural programming:
+
+  - OOP is faster and easier to execute
+  - OOP provides a clear structure for the programs
+  - OOP helps to keep the C++ code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+  - OOP makes it possible to create full reusable applications with less code and shorter development time */
+
+  class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -5,16 +5,6 @@
 #include <cmath>
 using namespace std;
 
-
-// C++ is Object Oriented Programming
-
-/* Object-oriented programming has several advantages over procedural programming:
-
-  - OOP is faster and easier to execute
-  - OOP provides a clear structure for the programs
-  - OOP helps to keep the C++ code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
-  - OOP makes it possible to create full reusable applications with less code and shorter development time */
-
 // FUNCTION DECLARATION
 void myFunction() {
   cout << "I just got executed!";
